500f0ab092219d326d63983c1a7a89f3112f97a3
sjj <826904340@qq.com>
2020-07-23 17:40:32 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,18 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+      title: 'hello world'
+    }
+  },
+  render (createElement) {
+    return createElement('h1', this.title)
+  }
+}
+</script>
